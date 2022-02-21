e575c069759bba22f016d9f9faf1ad6fc77cd7b6
Chue Chang <chuechang1996@outlook.com>
2022-02-21 15:58:05 -0600

    work on seed and main handlebar

--- seeds/galleryData.js
+++ seeds/galleryData.js
@@ -2,22 +2,25 @@ const { Gallery } = require('../models');
 
 const gallerydata = [
   {
-    name: 'Printemps',
-    starting_date: 'April 20, 2021 07:00:00',
-    ending_date: 'June 21, 2021 17:00:00',
+    name: 'PROJECTS',
+    starting_date: 'June 22, 2021 09:00:00',
+    ending_date: 'September 22, 2021 22:00:00',
+    weatherapp: 'https://luckylobster25.github.io/weather/',
+    techblog: 'https://thawing-peak-67886.herokuapp.com/',
+    gamereview: 'https://mighty-beyond-89277.herokuapp.com/'
   },
   {
-    name: 'Sommer',
+    name: 'CONNECT WITH ME',
     starting_date: 'June 22, 2021 09:00:00',
     ending_date: 'September 22, 2021 22:00:00',
   },
   {
-    name: 'Herfst',
+    name: 'RESUME',
     starting_date: 'September 23, 2021 08:30:00',
     ending_date: 'December 21, 2021 20:30:00',
   },
   {
-    name: 'Invierno',
+    name: 'ABOUT ME',
     starting_date: 'December 22, 2020 11:00:00',
     ending_date: 'March 19, 2021 19:00:00',
   },
